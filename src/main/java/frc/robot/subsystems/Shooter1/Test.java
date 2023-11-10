d8dc6d8de39774c10d2627d891b3c2bc2b55d1fb
AaronC75 <acac5757@icloud.com>
2023-11-09 16:07:04 -0800

    Math

--- src/main/java/frc/robot/subsystems/Shooter1/Test.java
+++ src/main/java/frc/robot/subsystems/Shooter1/Test.java
@@ -1,12 +1,18 @@
 package frc.robot.subsystems.Shooter1;
 
-public class Shooter1{
-    public static void main(String args[]
-    
+public class Test{
+    public void main(String args[]){
 
             // stuff goes here - Aaron
+        int answer = addnumbers(2,5);
+
+    }
+    
+    int addnumbers(int number1, int number2){
+        return number1 + number2;
+    }
 
 
     
-    )
+    
 }
