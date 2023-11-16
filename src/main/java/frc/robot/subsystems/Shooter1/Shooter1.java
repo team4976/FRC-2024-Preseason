2eee9ebe1a46fa3f8909a4cf170596791d6c5099
blade9540 <149724929+blade9540@users.noreply.github.com>
2023-11-16 16:46:50 -0500

    Create Shooter1.java

---
+++ src/main/java/frc/robot/subsystems/Shooter1/Shooter1.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.Shooter1;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter1 extends SubsystemBase {
+    public Shooter1() {}
+
+public class Shooter1{
+    public void main(String args[]){
+
+
+        
+            // stuff goes here 
+        int answer = addnumbers(2,5);
+
+    }
+    
+    int addnumbers(int number1, int number2){
+        return number1 + number2;
+    }
+
+
+    
+    
+}
