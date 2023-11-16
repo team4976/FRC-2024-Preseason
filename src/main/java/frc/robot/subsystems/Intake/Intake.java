9f5539eb89802f575bf0247d782d7cb1ccba53c2
Needlemouse4 <126884639+Needlemouse4@users.noreply.github.com>
2023-11-16 18:39:22 -0500

    Intake code is done (I think)

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,4 +1,6 @@
 package frc.robot.subsystems.Intake;
+import java.sql.CallableStatement;
+
 import com.ctre.pheonix.motorcontrol.can.TalonSRX;
 
 //How about txt.java
@@ -6,31 +8,45 @@ public class Intake {
     public static void main(String args[]) {
         //The lI1 class not to be confused with the Il1, the I1l, or the l1I classes.
         public class lI1 extends CommandBase {
-            public final TalonSRX motor1 = new TalonSRX (deviceNumber: 1);
-
+            //These could be moved to constants folder
+            //Declared motor, they uses 41 and 45
+            public final TalonSRX motor1 = new TalonSRX (deviceNumber: 41);
+            public final TalonSRX motor2 = new TalonSRX (deviceNumber: 45);
             public lI1() {
-        //Your code here
+        
             }
         
             @Override
             public void initialize() {
-        //Your code here        
+        //Your code here
+        //Not being used        
             }
         
             @Override
             public void execute() {
-            //Make the motors rotate. One reversed and the other normal
+            //Editing motor settings
+            public static final TalonSRX m_IntakeTalonLeft = new TalonSRX(41);
+            public static final TalonSRX m_IntakeTalonRight = new TalonSRX(45);
+            //Declare motors
+            m_IntakeTalonRight.setInverted(true);
+            //OneInvertedIntakeMotor
+            m_IntakeTalonLeft.set(ControlMode.PercentOutput,outputValue: 0.5);
+            m_IntakeTalonRight.set(ControlMode.PercentOutput,outputValue: 0.5);
+            //Output %
             
+            //I still don't know how to turn a motor
             }
         
             @Override
             public boolean isFinished() {
-        //Your code here        
+        //Your code here    
+        //Unused    
             }
         
             @Override
             public void end(boolean interrupted) {
         //Your code here
+        //Unused
             }
         }
         
