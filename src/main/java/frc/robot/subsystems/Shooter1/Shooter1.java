d77353857736c3850a179c3ca1ff8e19322ecdc1
AaronC75 <acac5757@icloud.com>
2023-11-16 14:44:06 -0800

    Update Shooter1.java

--- src/main/java/frc/robot/subsystems/Shooter1/Shooter1.java
+++ src/main/java/frc/robot/subsystems/Shooter1/Shooter1.java
@@ -6,17 +6,17 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Shooter1 extends SubsystemBase {
     public Shooter1() {}
     public boolean shooter1Shoot = false;
-}
+        
 public class Shooter1{
     public void main(String args[]){
 
             // stuff goes here - Aaron
-        int answer = addnumbers(2,5);
+        
 
     }
     
-    int addnumbers(int number1, int number2){
-        return number1 + number2;
+    {
+        
     }
 
 }
