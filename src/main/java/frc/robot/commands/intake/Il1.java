b03bdc533985fdb6cccb2ce31df92f83c478fe2f
Needlemouse4 <126884639+Needlemouse4@users.noreply.github.com>
2023-11-16 17:33:58 -0500

    We did enough work for today -RAMinflames (The computer didn't like us after the RAM was in flames)

--- src/main/java/frc/robot/commands/intake/Il1.java
+++ src/main/java/frc/robot/commands/intake/Il1.java
@@ -1,5 +1,5 @@
 package frc.robot.commands.intake;
 
-public class commandname {
+public class Il1 {
     
 }
--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,8 +1,38 @@
 package frc.robot.subsystems.Intake;
+import com.ctre.pheonix.motorcontrol.can.TalonSRX;
+
 //How about txt.java
 public class Intake {
     public static void main(String args[]) {
-        //How do I turn a motor?
+        //The lI1 class not to be confused with the Il1, the I1l, or the l1I classes.
+        public class lI1 extends CommandBase {
+            public final TalonSRX motor1 = new TalonSRX (deviceNumber: 1);
+
+            public lI1() {
+        //Your code here
+            }
+        
+            @Override
+            public void initialize() {
+        //Your code here        
+            }
+        
+            @Override
+            public void execute() {
+            //Make the motors rotate. One reversed and the other normal
+            
+            }
+        
+            @Override
+            public boolean isFinished() {
+        //Your code here        
+            }
+        
+            @Override
+            public void end(boolean interrupted) {
+        //Your code here
+            }
+        }
         
     }
 }
