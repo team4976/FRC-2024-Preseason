1e57f70c3138c03fff0023044ba1ab40e79d0c6e
Needlemouse4 <126884639+Needlemouse4@users.noreply.github.com>
2023-11-16 18:49:13 -0500

    Final edits for the day

--- src/main/java/frc/robot/commands/intake/lI1.java
+++ src/main/java/frc/robot/commands/intake/lI1.java
@@ -1,5 +1,5 @@
 package frc.robot.commands.intake;
 
 public class lI1 {
-    
+    //turn motor command goes here
 }
--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -4,15 +4,14 @@ import java.sql.CallableStatement;
 
 import com.ctre.pheonix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
 //How about txt.java
 public class Intake {
     public static void main(String args[]) {
         //The lI1 class not to be confused with the Il1, the I1l, or the l1I classes.
         public class lI1 extends CommandBase {
-            //These could be moved to constants folder
-            //Declared motor, they uses 41 and 45
-            public final TalonSRX motor1 = new TalonSRX (deviceNumber: 41);
-            public final TalonSRX motor2 = new TalonSRX (deviceNumber: 45);
+            
             public lI1() {
         
             }
@@ -26,15 +25,16 @@ public class Intake {
             @Override
             public void execute() {
             //Editing motor settings
-            public static final TalonSRX m_IntakeTalonLeft = new TalonSRX(41);
-            public static final TalonSRX m_IntakeTalonRight = new TalonSRX(45);
+            //These could be moved to constants folder
+            //Declared motor, they uses 41 and 45
+            public static final TalonSRX m_IntakeTalonLeft = new TalonSRX(deviceNumber: 41);
+            public static final TalonSRX m_IntakeTalonRight = new TalonSRX(deviceNumber: 45);
             //Declare motors
             m_IntakeTalonRight.setInverted(true);
             //OneInvertedIntakeMotor
             m_IntakeTalonLeft.set(ControlMode.PercentOutput,outputValue: 0.5);
             m_IntakeTalonRight.set(ControlMode.PercentOutput,outputValue: 0.5);
             //Output %
-            
             //I still don't know how to turn a motor
             }
         
