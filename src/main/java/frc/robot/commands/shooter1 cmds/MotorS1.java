aeaece80c5e28afbaa7c957113ad60803af9c8d8
blade9540 <149724929+blade9540@users.noreply.github.com>
2023-11-16 18:34:38 -0500

    Update MotorS1.java

--- src/main/java/frc/robot/commands/shooter1 cmds/MotorS1.java
+++ src/main/java/frc/robot/commands/shooter1 cmds/MotorS1.java
@@ -2,3 +2,8 @@ package frc.robot.commands.shooter1cmds.MotorS1.;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class MotorS1{
+
+    
+}
