efd3121477350e872d59a2da2bc9f4cafc8e1981
AaronC75 <acac5757@icloud.com>
2023-11-30 15:28:46 -0800

    Update Shooter1.java

--- src/main/java/frc/robot/subsystems/Shooter1/Shooter1.java
+++ src/main/java/frc/robot/subsystems/Shooter1/Shooter1.java
@@ -4,37 +4,35 @@ import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.shooter1Cmds.MotorS1;
 
 public class Shooter1 extends SubsystemBase {
 
     public static final Talon MotorSh1 = new TalonSRX(deviceNumber:45); 
-    public boolean shooterOneShoot = false;
-
+    public static boolean shooterOneShoot = false;
+    CommandBase motor1 = new MotorS1();
     public void main(String args[]){
 
             // stuff goes here - Aaron
-            CommandXboxController exampleCommandController = new CommandXboxController(1);
-            exampleCommandController.x(shooterOneShoot.onTrue).onTrue(MotorS1,shooterOneShoot = true);
-            exampleCommandController.x(shooterOneShoot.onTrue).onTrue(MotorS1);
+            CommandXboxController  exampleCommandController = new CommandXboxController(1);
+            exampleCommandController.x().onTrue(motor1);
             
-
     }
 //if(u==true){
 //
 //}
-    static public void shoot(){
-        MotorSh1.set(18);
-        System.out.println("Shoot Ball");
-
-    }
-
-    static public void stop(){
-    MotorSh1.set(0);
-    }
-    
-    {
+    public static void shoot(){
+        if(shooterOneShoot==true){
         
+            MotorSh1.set(18);
+            System.out.println("Shoot Ball");
+        }
+       
     }
+    public static void stop(){
+        if(shooterOneShoot==false) {
+            MotorSh1.set(0);  
+        } 
 
 }
 
